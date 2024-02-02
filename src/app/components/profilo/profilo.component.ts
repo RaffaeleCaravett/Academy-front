7d6708906a9a00d109badcbb98f9ce77c0f07beb
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-02-02 12:56:59 +0100

    corsi ended

--- src/app/components/profilo/profilo.component.ts
+++ src/app/components/profilo/profilo.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profilo',
   templateUrl: './profilo.component.html',
   styleUrls: ['./profilo.component.scss']
 })
-export class ProfiloComponent {
+export class ProfiloComponent implements OnInit{
+user:any
 
+constructor(){
+
+}
+  ngOnInit(): void {
+if(localStorage.getItem('user'))
+{
+  this.user=JSON.parse(localStorage.getItem('user')!)
+}
+}
 }
